7ffdc8c1901d72022ecd0c5c5f82a2777230c041
Aadit Yadav <aadityadav121@gmail.com>
2025-11-25 18:27:47 +0530

    DOM assgnment-03 done and dusted

---
+++ DOM-3/script.js
@@ -0,0 +1,24 @@
+let btn = document.querySelector("button");
+let h2 = document.querySelector("h2");
+let inner = document.querySelector(".inner");
+
+let count = 0;
+
+btn.addEventListener("click", () => {
+  btn.style.pointerEvents = "none";
+
+  let num = Math.floor(Math.random() * 100);
+
+  let id = setInterval(() => {
+    count++;
+    h2.innerHTML = count + "%";
+    inner.style.width = count + "%";
+    btn.style.opacity = "0.6";
+  }, num);
+
+  setTimeout(() => {
+    clearInterval(id);
+    btn.innerHTML = "Downloaded";
+    console.log(`Downloaded in ${num / 10} seconds`);
+  }, num * 100);
+});
