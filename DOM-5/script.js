80d1bdde6d5e8e9c49a247843e42cbc30272be78
Aadit Yadav <aadityadav121@gmail.com>
2025-11-29 23:42:21 +0530

    DOM assignment-5 Done and Dusted

--- DOM-5/script.js
+++ DOM-5/script.js
@@ -1,1 +1,34 @@
-console.log("working");
+const users = [
+  {
+    image: "./CC-Carousel-750x424.png",
+    title: "Clash Of Clans",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    image: "./DBZ Black Goku.jpg",
+    title: "Dragon Ball Z",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    image: "./Naruto Kakashi.jpg",
+    title: "Naruto",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    image: "./Spidy (2).jpg",
+    title: "Spider-man",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+];
+let main = document.querySelector("main");
+let card = "";
+users.forEach((elem) => {
+  card += `<div class="card">
+            <img src="${elem.image}" alt="" />
+            <h3>${elem.title}</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>`;
+
+});
+
+main.innerHTML = card;
